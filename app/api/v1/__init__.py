d1a22eab9cf2b5043a3b201742f4f46ea826fd58
Julius Siregar <juliussiregar1011@gmail.com>
2024-11-14 11:01:04 +0700

    feat: fitur daily salary

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -6,6 +6,7 @@ from app.api.v1.user_mgt import router as user_router
 from app.api.v1.position_mgt import router as position_router
 from app.api.v1.company_mgt import router as company_router
 from app.api.v1.employee_mgt import router as employee_router
+from app.api.v1.dailySalary_mgt import router as dailySalary_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(auth_router)
@@ -13,3 +14,4 @@ router.include_router(user_router)
 router.include_router(position_router)
 router.include_router(company_router)
 router.include_router(employee_router)
+router.include_router(dailySalary_router)
