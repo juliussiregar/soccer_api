881d0f498a7fc1937022b145a676a8ca654ce990
Julius Siregar <juliussiregar1011@gmail.com>
2024-12-19 11:23:03 +0700

    test

---
+++ app/models/__init__.py
@@ -0,0 +1,14 @@
+from .base import Base
+
+from app.models.audit import ApiLog
+from app.models.application import Application
+from app.models.attendance import Attendance
+from app.models.company import Company
+from app.models.daily_salary import DailySalary
+from app.models.employee import Employee
+from app.models.employee_daily_salary import EmployeeDailySalary
+from app.models.employee_monthly_salary import EmployeeMonthlySalary
+from app.models.position import Position
+from app.models.revoked_token import RevokedToken
+from app.models.user import User
+from app.models.face import Face
